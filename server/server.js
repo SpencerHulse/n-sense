53a52e79053a20027c13e0debfc8fc0d8a2dafb6
Spencer Hulse <hulse.spencer@gmail.com>
2022-05-14 17:27:25 -0400

    Added basic User model and fixed error in server.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const db = require("./config/connection");
+const PORT = process.env.PORT || 3001;
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
