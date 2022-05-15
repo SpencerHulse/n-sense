c9b4f066eb6e2c35cf18736bd8ce20a9a2c941b4
Spencer Hulse <hulse.spencer@gmail.com>
2022-05-15 18:34:36 -0400

    Got test App.js, page, and component set up. Set up basic Apollo Server in App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,51 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Pages
+import TestPage from "./pages/TestPage";
+import NoMatch from "./pages/NoMatch";
+
+// Apollo
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
-  return <div></div>;
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={TestPage} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
---
+++ client/src/components/Testing/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Testing() {
+  return (
+    <div>
+      <h2>Testing Component</h2>
+    </div>
+  );
+}
+
+export default Testing;
