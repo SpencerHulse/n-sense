5156a65f70f06137dfcab8f5f7e41b650b124c6a
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-05-15 21:45:59 -0500

    changes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,10 +3,28 @@ import React, { useEffect } from "react";
 function Nav(props) {
   const { setCurrentNav, currentNav } = props;
   return (
-    <header className="flex-row px-1">
-      <div className="flex-row logo-container flex justify-between items-center">
-        <h1>N-Sense</h1>
-      </div>
+    <header>
+      <nav className="flex flew-row">
+        <div className="flex-row logo-container flex border-red border-4">
+          <h1 className="underline">N-Sense</h1>
+        </div>
+        <div className="top-nav">
+          <a
+            href="#cart"
+            className="nav-link"
+            onClick={() => setCurrentNav("cart")}
+          >
+            Cart
+          </a>
+          <a
+            href="#about"
+            className="nav-link"
+            onClick={() => setCurrentNav("about")}
+          >
+            About
+          </a>
+        </div>
+      </nav>
     </header>
   );
 }
