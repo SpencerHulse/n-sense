b610ccacacb049f27b8f35d379145f458dbb45c9
Spencer Hulse <hulse.spencer@gmail.com>
2022-05-15 21:36:09 -0400

    Added queries.js for graphql and added tests to TestPage.js.

--- client/src/pages/TestPage.js
+++ client/src/pages/TestPage.js
@@ -1,7 +1,34 @@
 import React from "react";
 import Testing from "../components/Testing";
 
+import { useQuery } from "@apollo/client";
+import {
+  QUERY_USERS,
+  QUERY_CATEGORIES,
+  QUERY_PRODUCTS,
+  QUERY_PRODUCT,
+  // QUERY_ORDER,
+} from "../utils/queries";
+
 const TestPage = () => {
+  const { data: usersData } = useQuery(QUERY_USERS);
+  const { data: categoriesData } = useQuery(QUERY_CATEGORIES);
+  const { data: productsData } = useQuery(QUERY_PRODUCTS);
+  const { data: productData } = useQuery(QUERY_PRODUCT, {
+    variables: { id: "62805f273b57b327c88436c3" },
+  });
+  // Requires an order
+  // const { data: orderData } = useQuery(QUERY_ORDER);
+
+  const allQueryData = {
+    usersData,
+    categoriesData,
+    productsData,
+    productData,
+  };
+
+  console.log(allQueryData);
+
   return (
     <div>
       <h2>Test Page!</h2>
