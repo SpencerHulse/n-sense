93fd4013787c578a07c3c982ff3d9a5f64e0368d
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-05-18 20:05:38 -0500

    carousel beginnings

--- client/src/components/ScrollCard/index.js
+++ client/src/components/ScrollCard/index.js
@@ -1,24 +1,30 @@
-{
-  <div className="product-card inline-block px-3">
-    <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-      <div className="w-full h-1/2 bg-slate-300">
-        <a href="#">
-          <img className="rounded-t-lg scale-50" src={candle} alt="candle" />
+import React from "react";
+
+function ScrollCard({ _id, name, description, price, primaryImage, category }) {
+  return (
+    <div className="product-card flex justify-center px-3 ">
+      <div className="product-card rounded-lg shadow-lg border border-gray-200 bg-white max-w-xs w-64 h-64 dark:bg-gray-800 dark:border-gray-700  overflow-hidden hover:shadow-xl transition-shadow duration-300 ease-in-out">
+        <a href="#!">
+          <img
+            className="rounded-t-lg"
+            src={require(`../../assets/images/${primaryImage}.jpg`)}
+            alt={`${name} ${category}`}
+          />
         </a>
-      </div>
-      <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Oogly Moogly Wax in Jar
-          </h5>
-        </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          100% Handmade from Lord Chris himself!
-        </p>
-        <div className="flex flex-row justify-between w-full">
-          <p className="price text-xl font-bold text-white">$6.66</p>
+        <div className="card-info bg-slate-300 w-full h-1/2">
+          <a href="#">
+            <h5 className="text-gray-900 text-2xl tracking-tight font-medium mb-2 font-bold dark:text-white">
+              {name} {category}
+            </h5>
+          </a>
+          <div className="category mb-3 font-normal text-gray-700 dark:text-gray-400 ">
+            <p className="text-gray-700 text-base pb-4">{description}</p>
+          </div>
+          <p className="price text-xl font-bold text-white">${price}</p>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
+
+export default ScrollCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ import { updateProducts } from "../features/productSlice";
 // Apollo/GraphQL
 import { useQuery } from "@apollo/client";
 import { QUERY_PRODUCTS } from "../utils/queries";
+import ScrollCardCarousel from "../components/ScrollCardCarousel";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -32,88 +33,10 @@ const Home = () => {
 
   return (
     <div className="container">
-      {/*     <div className="h-96 bg-slate-300">
-        <h1>Carousel</h1>
-      </div>
-      <div className="new-products h-96">
-        <div className="flex flex-col bg-white m-auto p-auto w-full h-96">
-          <h1 className="flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800">
-            New and Worthy of Chris's Love
-          </h1>
-          <div className="flex overflow-x-scroll pb-10 hide-scroll-bar">
-            <div className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
-              <div className="product-card inline-block px-3">
-                <div className="w-64 h-64 max-w-sm overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  <div className="w-full h-1/2 bg-slate-300">
-                    <a href="#">
-                      <img
-                        className="rounded-t-lg scale-50"
-                        src={candle}
-                        alt="candle"
-                      />
-                    </a>
-                  </div>
-                  <div className="p-5">
-                    <a href="#">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Oogly Moogly Wax in Jar
-                      </h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                      100% Handmade from Lord Chris himself!
-                    </p>
-                    <div className="flex flex-row justify-between w-full">
-                      <p className="price text-xl font-bold text-white">
-                        $6.66
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div className="inline-block px-3">
-                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  Card
-                </div>
-              </div>
-              <div className="inline-block px-3">
-                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  Card
-                </div>
-              </div>
-              <div className="inline-block px-3">
-                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  Card
-                </div>
-              </div>
-              <div className="inline-block px-3">
-                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  Card
-                </div>
-              </div>
-              <div className="inline-block px-3">
-                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  Card
-                </div>
-              </div>
-              <div className="inline-block px-3">
-                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  Card
-                </div>
-              </div>
-              <div className="inline-block px-3">
-                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                  Card
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
-      {/* <Carousel></Carousel> */}
       {products.length ? (
         // Needs to be flex
         <div className="">
+          <ScrollCardCarousel></ScrollCardCarousel>
           {filterProducts().map((product) => (
             <ProductCard
               key={product._id}
