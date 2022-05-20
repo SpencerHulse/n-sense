c24e307a8e508b1e8a0ceab8b8f7434717aba67d
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-05-20 11:46:14 -0500

    login page

--- client/src/components/ScrollCardCarousel/index.js
+++ client/src/components/ScrollCardCarousel/index.js
@@ -3,16 +3,21 @@ import ScrollCard from "../ScrollCard";
 
 function ScrollCardCarousel() {
   return (
-    <div className="flex overflow-x-scroll pb-10 hide-scroll-bar">
-      <div className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
-        <ScrollCard />
-        <ScrollCard />
-        <ScrollCard />
-        <ScrollCard />
-        <ScrollCard />
-        <ScrollCard />
-        <ScrollCard />
-        <ScrollCard />
+    <div className="container flex flex-col">
+      <h1 className="lg:ml-40 md:ml-20 ml-10 font-bold text-3xl">
+        New and Newsworthy
+      </h1>
+      <div className="flex overflow-x-scroll pb-10 hide-scroll-bar">
+        <div className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+          <ScrollCard />
+          <ScrollCard />
+          <ScrollCard />
+          <ScrollCard />
+          <ScrollCard />
+          <ScrollCard />
+          <ScrollCard />
+          <ScrollCard />
+        </div>
       </div>
     </div>
   );
