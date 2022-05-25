e6c40c2c066348c9dbbb507b54f54bb969a6ea18
Spencer Hulse <hulse.spencer@gmail.com>
2022-05-24 20:57:46 -0400

    Responsive Admin Dash.

--- client/src/pages/AdminDash.js
+++ client/src/pages/AdminDash.js
@@ -88,20 +88,17 @@ const Admin = () => {
     <>
       <div className="section">
         <div className="container mx-auto">
-
-          <h2 className="mb-10">Admin Dashboard</h2>
-
+          <h2 className="mb-6 text-center">Admin Dashboard</h2>
 
           {/* Thinking each could take up half the screen, and a form can pop up beneath depending on selection */}
           <div className="admin-box mx-auto">
-
             <h2>Categories</h2>
             <p className="mb-5">Select a category to remove</p>
 
             <div className="flex gap-4 w-full">
-
               <div className="w-8/12">
-                <select className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                <select
+                  className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   name="categories"
                   id="categories"
                   onChange={() => handleCategoryChange()}
@@ -115,14 +112,20 @@ const Admin = () => {
                 </select>
               </div>
               <div className="w-4/12">
-                <button className="w-full text-red-900 bg-white border border-red-300 focus:outline-none hover:bg-red-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={categoryDeleteHandler}>
+                <button
+                  className="w-full text-red-900 bg-white border border-red-300 focus:outline-none hover:bg-red-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  onClick={categoryDeleteHandler}
+                >
                   Delete Category
                 </button>
               </div>
             </div>
             <br />
             {formType !== "Add Category" && (
-              <button className="add-button" onClick={() => setFormType("Add Category")}>
+              <button
+                className="add-button"
+                onClick={() => setFormType("Add Category")}
+              >
                 Add New Category
               </button>
             )}
@@ -134,7 +137,12 @@ const Admin = () => {
             <p className="mb-5">Select a product to edit or remove</p>
             <div className="flex gap-4 w-full">
               <div className="w-8/12">
-                <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="products" id="products" onChange={() => handleProductChange()} >
+                <select
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  name="products"
+                  id="products"
+                  onChange={() => handleProductChange()}
+                >
                   <option value="">Select a Product</option>
                   {!productLoading &&
                     products.map((product, i) => (
@@ -145,26 +153,33 @@ const Admin = () => {
                 </select>
               </div>
               <div className="w-4/12">
-
-                <button className="w-full text-red-900 bg-white border border-red-300 focus:outline-none hover:bg-red-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={productDeleteHandler}>Delete Product</button>
+                <button
+                  className="w-full text-red-900 bg-white border border-red-300 focus:outline-none hover:bg-red-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                  onClick={productDeleteHandler}
+                >
+                  Delete Product
+                </button>
               </div>
             </div>
             <br />
             {formType !== "Add Product" && (
-              <button className="add-button" onClick={() => setFormType("Add Product")}>
+              <button
+                className="add-button"
+                onClick={() => setFormType("Add Product")}
+              >
                 Add New Product
               </button>
             )}
-            {formType === "Add Product" && <ProductForm categories={categories} />}
+            {formType === "Add Product" && (
+              <ProductForm categories={categories} />
+            )}
             {formType === "Update Product" && selectedProduct && (
               <UpdateProduct
                 selectedProduct={selectedProduct}
                 categories={categories}
               />
             )}
-
           </div>
-
         </div>
       </div>
     </>
