1ebe6d442b51881c4a5742a59526792587470040
Spencer Hulse <hulse.spencer@gmail.com>
2022-05-24 23:46:51 -0400

    Made the 404 page work on dark mode.

--- client/src/pages/NoMatch.js
+++ client/src/pages/NoMatch.js
@@ -6,7 +6,7 @@ const NoMatch = () => {
     <div>
       <div className="section">
         <div className="container mx-auto">
-          <div className="minidialog mx-auto">
+          <div className="minidialog mx-auto dark:bg-[#494949]">
             <div className="minicontent mx-auto">
               <div className="items-center">
                 <img
@@ -14,15 +14,15 @@ const NoMatch = () => {
                   src={require(`../assets/images/nsense-logo.png`)}
                   alt="n-Sense logo"
                 />
-                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-white">
                   This page does not exist!
                 </h2>
-                <p className="mt-2 mb-5 text-center text-sm text-gray-600">
+                <p className="mt-2 mb-5 text-center text-sm text-gray-600 dark:text-[#bababa]">
                   Find it frustrating? Try one of our excellent incense scents
                   to relax, man.
                 </p>
                 <div>
-                  <p className="mt-2 text-center text-sm text-gray-600">
+                  <p className="mt-2 text-center text-sm text-green-700 hover:text-green-700 dark:text-green-600">
                     <Link to="/">Start shopping</Link>
                   </p>
                 </div>
